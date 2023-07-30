fd2f73911e317bc92f8cecb8d3476e20f2ecfde1
ankit030 <ankit.kumar30@delhivery.com>
2023-07-31 00:49:29 +0530

    MODIFIED MAIN PAGE

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,21 +1,3 @@
-// import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
-// @Component({
-//   selector: 'app-main-page',
-//   templateUrl: './main-page.component.html',
-//   styleUrls: ['./main-page.component.css']
-// })
-// export class MainPageComponent {
-//   // constructor(private router: Router) {}
-//   title = 'main-form-app';
-
-//   // openClientForm(clientNumber: number): void {
-//   //   // Navigate to the respective client's form page
-//   //   this.router.navigate(['/client-form', clientNumber]);
-//   // }
-// }
-
 
 import { Component } from '@angular/core';
 
@@ -24,16 +6,8 @@ import { Component } from '@angular/core';
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-// export class MainPageComponent {
-//   title = 'main-form-app';
-// }
+
 
 export class MainPageComponent {
-  constructor(private router: Router) {}
-
-  goToClientForm(clientNumber: number): void {
-    // Navigate to the corresponding client form page based on clientNumber
-    this.router.navigate(['/client' + clientNumber]);
-  }
 }
 
